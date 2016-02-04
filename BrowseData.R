ae5be4f533912e162fd84c37d82c1377d7f651ff
jaywarrick <jay.w.warrick@gmail.com>
2016-02-03 22:45:31 -0600

    WIP

--- BrowseData.R
+++ BrowseData.R
@@ -3,8 +3,14 @@ source('~/.Rprofile')
 sourceGitHubFile(user='jaywarrick', repo='R-Cytoprofiling', branch='master', file='PreProcessingHelpers.R')
 
 library(foreign)
-fileTable <- read.arff('/Users/jaywarrick/Documents/JEX/Feature Extraction/temp/JEXData0000000053.arff')
+fileTable <- read.arff('/Volumes/JEX Cruncher/JEX Databases/Dominique/temp/JEXData0000000022.arff')
 
+# Need to set groupCols as "everything but" Value and ImageChannel
+# Should be simple, just make vector of names... !(names(duh) %in% c('Value','ImageChannel'))
+# temp <- c('a','b')
+# duh[,mean(c),by=temp]
 shinyData <- getFileTable(fileTable)
 
+
 browseShinyData()
+
