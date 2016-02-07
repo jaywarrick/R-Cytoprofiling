569773dbf9b816a801674a1035702bad883aacfe
jaywarrick <jay.w.warrick@gmail.com>
2016-02-07 12:34:34 -0600

    Nearing something meaningful

--- BrowseData.R
+++ BrowseData.R
@@ -6,58 +6,102 @@ sourceGitHubFile(user='jaywarrick', repo='R-Cytoprofiling', branch='master', fil
 library(foreign)
 fileTable <- read.arff('/Volumes/JEX Cruncher/JEX Databases/Dominique/temp/JEXData0000000000.arff')
 
-# Preprocess the data
+##### DATA PREPROCESSING #####
+
+# Read in the data into a single table
 x1a <- fread(fileTable$Value[1])
 x1a$Class <- 'MT'
 x1b <- fread(fileTable$Value[2])
 x1b$Class <- 'WT'
 x1 <- rbindlist(list(x1a,x1b), use.names = TRUE)
+
+# Fix some naming issues from ImageJ
 x1 <- fixColNames(x1)
 x1 <- fixMeasurementNames(x1)
+
+# Make things easier to peruse
 setorder(x1, Id, Label, MaskChannel, Measurement, ImageChannel)
-x1$Id <- as.character(x1$Id)
+x1$Id <- as.character(x1$Id) # Void standardizing the Id
+
+# Remove measures that vascilate between 180 deg and 0 deg (i.e., no diff but numberwise they have a non-zero difference)
 x2 <- removeMeasurementNamesContaining(x1, "Phase_Order_2_Rep_0")
 x2 <- removeMeasurementNamesContaining(x2, "Phase_Order_4_Rep_0")
-x3 <- standardizeLongData(x2)
-diffs <- getDifferences(x3)
+
+# Tempororarily make the table wide to calculate averages of Haralick over the different directions
+x2b <- getWideTable(x2)
+x2b <- calculateRMSofHaralick(x2b)
+
+# Get our long table back and reorder
+x3 <- getLongTableFromTemplate(x2b, x2)
+setorder(x3, Id, Label, MaskChannel, Measurement, ImageChannel)
+
+# Perform robust standardization (x-median)/mad (entertain idea of not applying to histogram bins)
+x3 <- standardizeLongData(x3)
+
+# Generate a table of differences between measures for each MaskChannel/ImageChannel/Measurement combination
+diffs <- calculateChannelDifferences(x3)
+
+# Standardize the difference data
 diffs <- standardizeLongData(diffs)
+
+# Merge it with the original dataset, merging MaskChannel and ImageChannel into MeasurementName
 x4 <- rbindlist(list(x3,diffs), use.names = TRUE)
 x4$Measurement <- paste(x4$Measurement, x4$MaskChannel, x4$ImageChannel, sep='_')
 x4[,MaskChannel:=NULL]
 x4[,ImageChannel:=NULL]
+
+# Get a wide table for machine learning and plotting
 x5 <- getWideTable(x4)
+
+# Fix naming issues introduced by merging MaskChannel and ImageChannel names with Measurement name
 x5 <- fixColNames(x5)
 
-
-# Fix a few things for plotting etc
+# Convert ImRow and ImCol to numeric for looking at locational correlations
 x5$ImRow <- as.numeric(as.character(x5$ImRow))
 x5$ImCol <- as.numeric(as.character(x5$ImCol))
+
+# Generate a more informative text string for pt labels in plotly
 x5$cId <- paste0(x5$Id, ' RCClass[', x5$ImRow, ',', x5$ImCol, ',', x5$Class, ']')
 x5$cId <- paste0(x5$Id, ' RCClass[', x5$ImRow, ',', x5$ImCol, ',', x5$Class, ']')
+
+# Perform final sorting of columns of data for easier perusing
 x5 <- sortColsByName(x5)
 shinyData <- x5
 
+# Write the data for potential analysis outside R
 write.csv(shinyData, file='/Users/jaywarrick/Documents/MMB/Grants/2016 - RO1 Cytoprofiling/test.csv')
 
 # Look at the data
 browseShinyData()
 
+###### RANDOM FOREST MACHINE LEARNING #####
 library(randomForest)
+
 # Need to remove names like Id, Label, ImRow, ImCol, Z,
 dataToTest <- shinyData
 dataToTest[, c('cId','Id','Label','ImCol','ImRow','Z'):=NULL]
 
-##### Try some subsets #####
+# The 560 channel is potentiall suspect due to image acquistion issues. Remove to avoid potential bias.
 dataToTest <- removeColNamesContaining(dataToTest, "560")
 dataToTest$Class <- as.factor(dataToTest$Class)
+
+# Set the random seed to reproduce results
 set.seed(416)
-rf <- randomForest(formula= Class ~ ., data=dataToTest, ntree=5, maxnodes=10, importance=TRUE, proximity=TRUE, do.trace=TRUE, keep.forest=TRUE)
+
+# Learn the trees
+rf <- randomForest(formula= Class ~ ., data=dataToTest, ntree=50, maxnodes=3, importance=TRUE, proximity=TRUE, do.trace=TRUE, keep.forest=TRUE)
+
+# Get an ordered table of importances (accuracy and gini)
 rfImp <- data.frame(rf$importance)
-rfImp <- rfImp[order(rfImp$MeanDecreaseGini, decreasing=TRUE),]
+rfImp <- rfImp[order(rfImp$MeanDecreaseAccuracy, decreasing=TRUE),]
+
+# Creat interactive plot to browse importance results
 library(plotly)
 plot_ly(rfImp, mode='markers', x=row.names(rfImp), y=rfImp$MeanDecreaseAccuracy, text=row.names(rfImp))
 
-##### Test for index issue ######
+##### DEBUGGING & TESTS #####
+
+#### Test for index issue #####
 order1 <- data.table(ImRow=0:5, ImCol=rep(0:5, each=6), index1=0:35)
 order2 <- data.table(ImRow=rep(0:5,each=6), ImCol=0:5, index2=0:35)
 order3 <- data.table(ImRow=0:5, ImCol=rep(0:5, each=6), index3=c(0:5,rev(6:11),12:17,rev(18:23),24:29,rev(30:35)))
@@ -84,5 +128,9 @@ plot(mt[,get('index2')], mt[,get(nameToGet2)])
 plot(mt[,get('index3')], mt[,get(nameToGet3)])
 # Yay, no trend!
 
+##### Debug stuff #####
+x2[Measurement=='HistogramBin_13' & MaskChannel=='WholeCell']
 
-
+MADs <- shinyData[,lapply(.SD, mad, na.rm=TRUE), .SDcols=getNumericCols(shinyData)]
+SDs <- shinyData[,lapply(.SD, sd, na.rm=TRUE), .SDcols=getNumericCols(shinyData)]
+SDs$net.imagej.ops.Ops.Stats.StdDev_WholeCell_650X705M
--- PreProcessingHelpers.R
+++ PreProcessingHelpers.R
@@ -1,6 +1,8 @@
 library(data.table)
 library(foreign)
 
+##### Visualization #####
+
 browseShinyData <- function()
 {
      sourceGitHubFile(user='jaywarrick', repo='R-General', branch='master', file='DataClassBrowser/ui.R')
@@ -8,46 +10,75 @@ browseShinyData <- function()
      shinyApp(ui=myUI, server=myServer)
 }
 
-getComboNames <- function(x, prefix='_')
+##### Table IO #####
+
+getXYCSVsAsTableFromDir <- function(dir, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
 {
-     if(length(x) < 2)
+     ret <- list()
+     fList <- list.files(path = dir, recursive = TRUE)
+     for(f in fList)
      {
-          return(NULL)
+          if((grepl('x', f) || grepl('y', f)) & grepl('.csv', f))
+          {
+               fileName <- strsplit(f, "\\.")[[1]][1]
+               ret[[fileName]] <- getXYCSVAsTable(dir, f, xName, xExpression, yName, yExpression)
+          }
      }
-     temp <- combn(x, 2)
-     #print(temp)
-     temp <- paste0(temp[1,],"_minus_",temp[2,])
-     temp <- paste0(prefix, temp)
-     return(temp)
+     retTable <- rbindlist(ret)
+     return(retTable)
 }
 
-getComboDifferences <- function(x)
+getXYCSVAsTable <- function(dir, file, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
 {
-     if(length(x) < 2)
-     {
-          return(NULL)
-     }
-     temp <- combn(x, 2)
-     temp <- temp[1,]-temp[2,]
-     return(temp)
+     fileName <- strsplit(file, "\\.")[[1]][1]
+     xy <- strsplit(fileName, "_")[[1]]
+     y <- as.numeric(substr(xy[1],2,nchar(xy[1])))
+     x <- as.numeric(substr(xy[2],2,nchar(xy[2])))
+     xVal <- eval(parse(text=xExpression))
+     yVal <- eval(parse(text=yExpression))
+     print(paste0('Reading ', file.path(dir,file), ' as ', xName, '=', xVal, ', ', yName, '=', yVal, '.'))
+     theTable <- fread(file.path(dir,file))
+     theTable[,(xName),with=FALSE] <- xVal
+     theTable[,(yName),with=FALSE] <- yVal
+     return(theTable)
 }
 
-getFileTable <- function(fileTable)
+getXYArffsAsTableFromDir <- function(dir, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
 {
-     temp <- data.table(read.arff(fileTable$Value[1]))
-     setorder(temp, Id, Label, MaskChannel, Measurement, ImageChannel)
-     return(temp)
+     ret <- list()
+     fList <- list.files(path = dir, recursive = TRUE)
+     for(f in fList)
+     {
+          if((grepl('x', f) || grepl('y', f)) & grepl('.arff', f))
+          {
+               fileName <- strsplit(f, "\\.")[[1]][1]
+               ret[[fileName]] <- getXYArffAsTable(dir, f, xName, xExpression, yName, yExpression)
+          }
+     }
+     retTable <- rbindlist(ret)
+     return(retTable)
 }
+
+getXYArffAsTable <- function(dir, file, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
+{
+     fileName <- strsplit(file, "\\.")[[1]][1]
+     xy <- strsplit(fileName, "_")[[1]]
+     y <- as.numeric(substr(xy[1],2,nchar(xy[1])))
+     x <- as.numeric(substr(xy[2],2,nchar(xy[2])))
+     xVal <- eval(parse(text=xExpression))
+     yVal <- eval(parse(text=yExpression))
+     print(paste0('Reading ', file.path(dir,file), ' as ', xName, '=', xVal, ', ', yName, '=', yVal, '.'))
+     theTable <- read.arff(file.path(dir,file))
+     theTable[,xName] <- xVal
+     theTable[,yName] <- yVal
+     return(data.table(theTable))
+}
+
+##### Wide Table Operations #####
 
 getColNamesContaining <- function(x, name)
 {
      return(names(x)[grepl(name,names(x))])
-}
-
-getMeasurementNamesContaining <- function(x, name)
-{
-     ms <- unique(x$Measurement)
-     return(ms[grepl(name,ms)])
 }
 
 removeColNamesContaining <- function(x, name)
@@ -62,131 +93,12 @@ removeColNamesContaining <- function(x, name)
      return(x)
 }
 
-removeMeasurementNamesContaining <- function(x, name)
-{
-     namesToRemove <- getMeasurementNamesContaining(x, name)
-     print("Removing the following Measurements...")
-     for(name in namesToRemove)
-     {
-          print(name)
-     }
-     x <- x[!(Measurement %in% namesToRemove)]
-     return(x)
-}
-
-removeNoVarianceCols <- function(x)
-{
-     namesToRemove <- getNoVarianceCols(x)
-     if(length(namesToRemove) > 0)
-     {
-          print("Removing cols with a variance of zero...")
-          for(name in namesToRemove)
-          {
-               print(name)
-               x[,(name):=NULL]
-          }
-     }
-}
-
-testFunc2 <- function(x, measurement)
-{
-     sdx <- sd(x, na.rm=TRUE)
-     if(is.na(sdx) || sdx == 0 || is.nan(sdx))
-     {
-          print(paste0("Removing zero variance measure: ", measurement, '.'))
-          return(NULL)
-     }else
-     {
-          return(x)
-     }
-}
-duh2 <- data.table(a=rep(1:3,each=3), b=c(1:3,c(1,1,1),1:3), c=c('a','b','c','d','e','f','g','h','i'))
-duh2[,list(Value=testFunc2(b, a)), by=c('a')]
-
-removeNoVarianceMeasurements <- function(x)
-{
-     # Get a vector with
-     temp <- x[,list(stdev=sd(Value)), by=c('MaskChannel','ImageChannel','Measurement')]
-     temp <- data.frame(temp[stdev == 0])
-     print("Removing measurements with 0 variance...")
-     print(temp)
-     x[,stdev:=sd(Value), by=c('MaskChannel','ImageChannel','Measurement')]
-     x <- x[stdev != 0]
-     x[, stdev:=NULL]
-     return(x)
-}
-
-getNoVarianceCols <- function(x)
-{
-     tempSD <- function(y){sd(y, na.rm = TRUE)}
-     tempNames <- x[,lapply(.SD, tempSD), .SDcols=getNumericCols(x)]
-     return(names(tempNames)[as.numeric(as.vector(tempNames))==0])
-}
-
-getDifferences <- function(x)
-{
-     if(length(unique(x$ImageChannel)) > 1)
-     {
-          # Calculate differences between channels
-          idCols <- getAllColNamesExcept(x, c('Value','ImageChannel'))
-          x2 <- x[,list(ImageChannel=getComboNames(ImageChannel), Value=getComboDifferences(Value)), by=idCols]
-     }else
-     {
-          # return an empty table with the same columns as provided
-          return(x[FALSE])
-     }
-}
-
-getWideTable <- function(x)
-{
-     idCols <- getAllColNamesExcept(x, c('Value','Measurement'))
-     x <- reorganize(x, idCols)
-     x <- sortColsByName(x);
-     return(x)
-}
-
-sortColsByName <- function(x)
-{
-     setcolorder(x, sort(names(x)))
-}
-
-standardizeWideData <- function(x)
-{
-     removeNoVarianceCols(x)
-     x[,lapply(.SD, function(x){if(is.numeric(x)){return(scale(x))}else{return(x)}})]
-}
-
-standardizeLongData <- function(x)
-{
-     x <- removeNoVarianceMeasurements(x)
-     x[,Value:=scale(Value),by=c('MaskChannel','ImageChannel','Measurement')]
-     return(x)
-}
-
-replaceStringInMeasurementNames <- function(x, old, new)
-{
-     x[,Measurement:=gsub(old,new,Measurement)]
-     return(x)
-}
-
-fixMeasurementNames <- function(x)
-{
-     #      x <- replaceStringInMeasurementNames(x, ' ', '')
-     #      x <- replaceStringInMeasurementNames(x, '\\$', '.')
-     #      x <- replaceStringInMeasurementNames(x, ':', '_')
-     #      return(x)
-     x[,Measurement:=gsub(' ','',Measurement)]
-     x[,Measurement:=gsub('\\$','.',Measurement)]
-     x[,Measurement:=gsub(':','_',Measurement)]
-     return(x)
-}
-
 fixColNames <- function(x)
 {
-	replaceStringInColNames(x, ' ', '')
-	replaceStringInColNames(x, '\\$', '.')
-	replaceStringInColNames(x, ':', '_')
-	return(x)
+     replaceStringInColNames(x, ' ', '')
+     replaceStringInColNames(x, '\\$', '.')
+     replaceStringInColNames(x, ':', '_')
+     return(x)
 }
 
 getAllColNamesExcept <- function(x, names)
@@ -209,65 +121,238 @@ replaceStringInColNames <- function(x, old, new)
      names(x) <- gsub(old, new, names(x))
 }
 
-getXYCSVsAsTableFromDir <- function(dir, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
+getWideTable <- function(x)
 {
-     ret <- list()
-     fList <- list.files(path = dir, recursive = TRUE)
-     for(f in fList)
+     idCols <- getAllColNamesExcept(x, c('Value','Measurement'))
+     x <- reorganize(x, idCols)
+     x <- sortColsByName(x);
+     return(x)
+}
+
+sortColsByName <- function(x)
+{
+     setcolorder(x, sort(names(x)))
+}
+
+standardizeWideData <- function(x)
+{
+     removeNoVarianceCols(x)
+     robustScale <- function(x)
      {
-          if((grepl('x', f) || grepl('y', f)) & grepl('.csv', f))
+          m <- median(x, na.rm=TRUE)
+          return((x-m)/mad(x, center=m, na.rm=TRUE))
+     }
+     x[,lapply(.SD, function(x){if(is.numeric(x)){return(robustScale(x))}else{return(x)}})]
+}
+
+removeNoVarianceCols <- function(x)
+{
+     namesToRemove <- getNoVarianceCols(x)
+     if(length(namesToRemove) > 0)
+     {
+          print("Removing cols with a variance of zero...")
+          for(name in namesToRemove)
           {
-          	fileName <- strsplit(f, "\\.")[[1]][1]
-          	ret[[fileName]] <- getXYCSVAsTable(dir, f, xName, xExpression, yName, yExpression)
+               print(name)
+               x[,(name):=NULL]
           }
      }
-     retTable <- rbindlist(ret)
-     return(retTable)
 }
 
-getXYCSVAsTable <- function(dir, file, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
+getNoVarianceCols <- function(x)
 {
-	fileName <- strsplit(file, "\\.")[[1]][1]
-	xy <- strsplit(fileName, "_")[[1]]
-	y <- as.numeric(substr(xy[1],2,nchar(xy[1])))
-	x <- as.numeric(substr(xy[2],2,nchar(xy[2])))
-	xVal <- eval(parse(text=xExpression))
-	yVal <- eval(parse(text=yExpression))
-	print(paste0('Reading ', file.path(dir,file), ' as ', xName, '=', xVal, ', ', yName, '=', yVal, '.'))
-	theTable <- fread(file.path(dir,file))
-	theTable[,(xName),with=FALSE] <- xVal
-	theTable[,(yName),with=FALSE] <- yVal
-	return(theTable)
+     tempSD <- function(y){sd(y, na.rm = TRUE)}
+     tempNames <- x[,lapply(.SD, tempSD), .SDcols=getNumericCols(x)]
+     return(names(tempNames)[as.numeric(as.vector(tempNames))==0])
 }
 
-getXYArffsAsTableFromDir <- function(dir, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
+##### Long Table Operations #####
+
+getLongTable <- function(x, idCols, measurementName='Measurement', valueName='Value')
 {
-     ret <- list()
-     fList <- list.files(path = dir, recursive = TRUE)
-     for(f in fList)
+     return(melt(x, getAllColNamesExcept(x, idCols), variable.name=measurementName, value.name=valueName, na.rm=TRUE))
+}
+
+getLongTableFromTemplate <- function(x, longTemplate)
+{
+     return(getLongTable(x, idCols=getAllColNamesExcept(x, getAllColNamesExcept(longTemplate, c('Measurement','Value')))))
+}
+
+getMeasurementNamesContaining <- function(x, name)
+{
+     ms <- unique(x$Measurement)
+     return(ms[grepl(name,ms)])
+}
+
+removeMeasurementNamesContaining <- function(x, name)
+{
+     namesToRemove <- getMeasurementNamesContaining(x, name)
+     print("Removing the following Measurements...")
+     for(name in namesToRemove)
      {
-          if((grepl('x', f) || grepl('y', f)) & grepl('.arff', f))
+          print(name)
+     }
+     x <- x[!(Measurement %in% namesToRemove)]
+     return(x)
+}
+
+standardizeLongData <- function(x, by=c('MaskChannel','ImageChannel','Measurement'))
+{
+     robustScale <- function(x)
+     {
+          m <- median(x, na.rm=TRUE)
+          return((x-m)/mad(x, center=m, na.rm=TRUE))
+     }
+     x <- removeNoMADMeasurements(x)
+     x[,Value:=robustScale(Value),by=by]
+     return(x)
+}
+
+removeNoVarianceMeasurements <- function(x, val='Value', by=c('MaskChannel','ImageChannel','Measurement'))
+{
+     # See if we have any columns to remove and record the info for reporting
+     temp <- x[,list(stdev=sd(get(val))), by=by]
+     temp <- data.frame(temp[stdev == 0])
+     print("Removing measurements with 0 variance...")
+     print(temp)
+     # Tempororarily add a column in the table with stdev in it
+     x[,stdev:=sd(get(val)), by=by]
+     y <- x[stdev != 0]
+     x[, stdev:=NULL]
+     y[, stdev:=NULL]
+     return(y)
+}
+
+removeNoMADMeasurements <- function(x, val='Value', by=c('MaskChannel','ImageChannel','Measurement'))
+{
+     # See if we have any columns to remove and record the info for reporting
+     temp <- x[,list(MAD=mad(get(val), na.rm=TRUE)), by=by]
+     temp <- data.frame(temp[MAD == 0])
+     print("Removing measurements with 0 MAD...")
+     print(temp)
+     # Tempororarily add a column in the table with stdev in it
+     x[,MAD:=mad(get(val), na.rm=TRUE), by=by]
+     y <- x[MAD != 0]
+     x[, MAD:=NULL]
+     y[, MAD:=NULL]
+     return(y)
+}
+
+removeNoVarianceMeasurements <- function(x, val='Value', by=c('MaskChannel','ImageChannel','Measurement'))
+{
+     # See if we have any columns to remove and record the info for reporting
+     temp <- x[,list(stdev=sd(get(val))), by=by]
+     temp <- data.frame(temp[stdev == 0])
+     print("Removing measurements with 0 variance...")
+     print(temp)
+     # Tempororarily add a column in the table with stdev in it
+     x[,stdev:=sd(get(val)), by=by]
+     y <- x[stdev != 0]
+     x[, stdev:=NULL]
+     y[, stdev:=NULL]
+     return(y)
+}
+
+replaceStringInMeasurementNames <- function(x, old, new)
+{
+     x[,Measurement:=gsub(old,new,Measurement)]
+     return(x)
+}
+
+fixMeasurementNames <- function(x)
+{
+     #      x <- replaceStringInMeasurementNames(x, ' ', '')
+     #      x <- replaceStringInMeasurementNames(x, '\\$', '.')
+     #      x <- replaceStringInMeasurementNames(x, ':', '_')
+     #      return(x)
+     x[,Measurement:=gsub(' ','',Measurement)]
+     x[,Measurement:=gsub('\\$','.',Measurement)]
+     x[,Measurement:=gsub(':','_',Measurement)]
+     return(x)
+}
+
+##### Feature Calculations #####
+
+calculateChannelDifferences <- function(x)
+{
+     if(length(unique(x$ImageChannel)) > 1)
+     {
+          # Calculate differences between channels
+          idCols <- getAllColNamesExcept(x, c('Value','ImageChannel'))
+          x2 <- x[,list(ImageChannel=getComboNames(ImageChannel), Value=getComboDifferences(Value)), by=idCols]
+     }else
+     {
+          # return an empty table with the same columns as provided
+          return(x[FALSE])
+     }
+}
+
+getComboNames <- function(x)
+{
+     if(length(x) < 2)
+     {
+          return(NULL)
+     }
+     temp <- combn(x, 2)
+     #print(temp)
+     temp <- paste0(temp[1,],"_minus_",temp[2,])
+     return(temp)
+}
+
+getComboDifferences <- function(x)
+{
+     if(length(x) < 2)
+     {
+          return(NULL)
+     }
+     temp <- combn(x, 2)
+     temp <- temp[1,]-temp[2,]
+     return(temp)
+}
+
+calculateRMSofHaralick <- function(x, removeOriginalHaralickMeasures=FALSE)
+{
+     # If keeping Haralick features, combine measures for each direction by averaging to make "rotationally invariant".
+     # Find all names with Horizontal in them
+     hNames <- getColNamesContaining(x, 'Horizontal')
+     vNames <- gsub("Horizontal", "Vertical", hNames)
+     dNames <- gsub("Horizontal", "Diagonal", hNames)
+     adNames <- gsub("Horizontal", "AntiDiagonal", hNames)
+     avgNames <- gsub("Horizontal", "Avg", hNames)
+
+     haralickNames <- data.frame(H=hNames, V=vNames, D=dNames, AD=adNames, avg=avgNames, stringsAsFactors=FALSE)
+     myfunc <- function(row, theNames)
+     {
+          return(mean(row[,theNames$H] + row[,theNames$V] + row[,theNames$D] + row[,theNames$AD]))
+     }
+
+     x <- data.frame(x)
+     for(i in 1:nrow(haralickNames))
+     {
+          x[,haralickNames[i,5]] <- (x[,haralickNames[i,1]] + x[,haralickNames[i,2]] + x[,haralickNames[i,3]] + x[,haralickNames[i,4]])/4
+          if(removeOriginalHaralickMeasures)
           {
-               fileName <- strsplit(f, "\\.")[[1]][1]
-               ret[[fileName]] <- getXYArffAsTable(dir, f, xName, xExpression, yName, yExpression)
+               x <- x[,!(names(x) %in% as.character(haralickNames[i,1:4]))]
           }
      }
-     retTable <- rbindlist(ret)
-     return(retTable)
+
+     return(data.table(x))
 }
 
-getXYArffAsTable <- function(dir, file, xName='SNR', xExpression='(x+1)', yName='BLUR', yExpression='(y+1)*0.05')
-{
-	fileName <- strsplit(file, "\\.")[[1]][1]
-	xy <- strsplit(fileName, "_")[[1]]
-	y <- as.numeric(substr(xy[1],2,nchar(xy[1])))
-	x <- as.numeric(substr(xy[2],2,nchar(xy[2])))
-	xVal <- eval(parse(text=xExpression))
-	yVal <- eval(parse(text=yExpression))
-	print(paste0('Reading ', file.path(dir,file), ' as ', xName, '=', xVal, ', ', yName, '=', yVal, '.'))
-	theTable <- read.arff(file.path(dir,file))
-	theTable[,xName] <- xVal
-	theTable[,yName] <- yVal
-	return(data.table(theTable))
-}
+##### Testing #####
+
+# testFunc2 <- function(x, measurement)
+# {
+#      sdx <- sd(x, na.rm=TRUE)
+#      if(is.na(sdx) || sdx == 0 || is.nan(sdx))
+#      {
+#           print(paste0("Removing zero variance measure: ", measurement, '.'))
+#           return(NULL)
+#      }else
+#      {
+#           return(x)
+#      }
+# }
+# duh2 <- data.table(a=rep(1:3,each=3), b=c(1:3,c(1,1,1),1:3), c=c('a','b','c','d','e','f','g','h','i'))
+# duh2[,list(Value=testFunc2(b, a)), by=c('a')]
 
